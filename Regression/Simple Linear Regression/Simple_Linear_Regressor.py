59919cbcdf3d0b7f6b5be7b88fec09413627278f
sai-teja-m <31622112+sai-teja-m@users.noreply.github.com>
2018-06-03 13:38:18 +0530

    Simple linear Regression

---
+++ Regression/Simple Linear Regression/Simple_Linear_Regressor.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun  3 12:34:28 2018
+
+@author: Sai Teja Mummadi
+"""
+#Simple linear Regression
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
